ed8d755dadbea7ca662a7d43439116c5f1680d87
Dewatered-Milou <3101uolim@gmail.com>
2025-09-15 03:00:31 +0800

    Feat: Add all actual frontend components and pages

--- nantou-hockey-website/vite.config.js
+++ nantou-hockey-website/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 這行是關鍵！請換成你的儲-存庫名稱
   base: '/nantou-hockey-website/',
 })
